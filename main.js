0006398d10ccedd4cff70488d67c35d3982a1a2a
Miqhtiedev <miqhtiedev@gmail.com>
2021-04-02 19:28:26 -0400

    stop leaking token and embed #applications

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
-const Discord = require('discord.js');
+if (process.env.NODE_ENV !== "production") require("dotenv").config();
+
+const Discord = require("discord.js");
 
 const client = new Discord.Client();
 
-const prefix = '>';
+const prefix = ">";
 
-client.once('ready', () => {
-    console.log('Umbra Bot is online!');
-});    
+client.once("ready", () => {
+  console.log("Umbra Bot is online!");
+});
 
-client.on('message', message => {
-    if(!message.content.startsWith(prefix) || message.author.bot) return;
+client.on("message", (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-    const args = message.content.slice(prefix.length).split(/ +/);
-    const command = args.shift().toLowerCase();
+  const args = message.content.slice(prefix.length).split(/ +/);
+  const command = args.shift().toLowerCase();
 
-    if(command === 'ping'){
-       message.channel.send('pong')
-    } else if (command == 'apply'){
-        message.channel.send('Apply For The Guild In #ᴀᴘᴘʟɪᴄᴀᴛɪᴏɴꜱ')
-    }
-    });
+  if (command === "ping") {
+    message.channel.send("pong");
+  } else if (command == "apply") {
+    message.channel.send(`Apply For The Guild In ${message.guild.channels.cache.find((c) => c.name === "ᴀᴘᴘʟɪᴄᴀᴛɪᴏɴꜱ")?.toString()}`);
+  }
+});
 
-
-
-client.login('Nzk3MDkwNjM3NDA0MzA3NTA2.X_hagQ.Jxu27t5zCweMXpI9Cbw8WO3j0NI');
+client.login(process.env.TOKEN);
